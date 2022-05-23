6284feb58c208096f598f07991f3c42a237ec6fe
Suleman <bsef19a041@pucit.edu.pk>
2022-05-23 08:06:00 +0500

    MainMenu Button Added

--- MyApplication2/app/src/main/java/com/example/myapplication/learnColors.java
+++ MyApplication2/app/src/main/java/com/example/myapplication/learnColors.java
@@ -3,12 +3,21 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class learnColors extends AppCompatActivity {
-
+Button backBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_learn_colors);
+        backBtn=findViewById(R.id.BackButton);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
